efc1c4eac309e8792f4e0aa2badb9f52a9c29d4d
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2018-08-10 11:31:11 +0000

    Added blowfish encrypt method.

--- ios/Classes/BlowfishNativePlugin.m
+++ ios/Classes/BlowfishNativePlugin.m
@@ -1,4 +1,5 @@
 #import "BlowfishNativePlugin.h"
+#import <CommonCrypto/CommonCryptor.h>
 
 @implementation BlowfishNativePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -15,6 +16,44 @@
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (NSData *)doBlowfish:(NSData *)dataIn
+               context:(CCOperation)kCCEncrypt_or_kCCDecrypt
+                   key:(NSData *)key
+               options:(CCOptions)options
+                    iv:(NSData *)iv
+                 error:(NSError **)error
+{
+    CCCryptorStatus ccStatus   = kCCSuccess;
+    size_t          cryptBytes = 0;
+    NSMutableData  *dataOut    = [NSMutableData dataWithLength:dataIn.length + kCCBlockSizeBlowfish];
+
+    ccStatus = CCCrypt( kCCEncrypt_or_kCCDecrypt,
+                       kCCAlgorithmBlowfish,
+                       options,
+                       key.bytes,
+                       key.length,
+                       (iv)?nil:iv.bytes,
+                       dataIn.bytes,
+                       dataIn.length,
+                       dataOut.mutableBytes,
+                       dataOut.length,
+                       &cryptBytes);
+
+    if (ccStatus == kCCSuccess) {
+        dataOut.length = cryptBytes;
+    }
+    else {
+        if (error) {
+            *error = [NSError errorWithDomain:@"kEncryptionError"
+                                         code:ccStatus
+                                     userInfo:nil];
+        }
+        dataOut = nil;
+    }
+
+    return dataOut;
 }
 
 @end
